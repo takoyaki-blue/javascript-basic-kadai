43113668d8b54686a6995dc648774f69230437d5
takoyaki-blue <145022380+takoyaki-blue@users.noreply.github.com>
2023-11-10 08:41:16 +0900

    kadai_009をアップロード

---
+++ kadai_009/holiday.js
@@ -0,0 +1,21 @@
+const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
+
+let i;
+
+// for文での繰り返し
+console.log('▽for文');
+for (i = 0;i <= 15; i++) {
+  console.log(holidays[i]);
+}
+
+// iの値をリセット
+i = 0;
+
+console.log(' ');
+
+// while文での繰り返し
+console.log('▽while文');
+while (i<=15) {
+  console.log(holidays[i]);
+  i = i+1;
+}
