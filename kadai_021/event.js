569133a024a8cee7963b14067a50aeebfbb3612f
takoyaki-blue <145022380+takoyaki-blue@users.noreply.github.com>
2023-12-01 13:47:51 +0900

    kadai_021をアップロード

---
+++ kadai_021/event.js
@@ -0,0 +1,11 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+// textというidを持つHTML要素を取得し、定数に代入する
+const text = document.getElementById('text');
+
+// ボタンが押された2秒後にtextの内容を変更する
+btn.addEventListener('click', () =>{
+    setTimeout(() => {
+    text.textContent = 'ボタンをクリックしました'
+}, 2000);
+});
